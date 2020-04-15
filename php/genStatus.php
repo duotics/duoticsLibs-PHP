b245a3e22f6d15ba2eee0b76bf38730ce67a5200
Daniel Banegas <dbanegasl@gmail.com>
2020-04-15 10:47:18 -0500

    upgrade bootstrap version to 4.4

--- php/genStatus.php
+++ php/genStatus.php
@@ -1,29 +1,53 @@
 <?php
 /*Funcion para visualizar status
 v.2.0 -> 2017-06-02 :: Buton Estado Gen
+v.3.0 -> 2019-11-13 :: Modify to DROP GLAPHICON AND CHANGE FOR fontawesome default
+v.3.1 -> add param icons
+v.3.2 -> change btn-xs -> to -> btn-sm
+v.4.0 -> agregado array de iconos como alternativa a iconos por defecto
+v.4.1 -> upgrade to boostrap 4.4 : change btn-xs -> to -> btn-sm
+			upgrade to fontawesome
 */
-function genStatus($dest,$params,$css=NULL){//v.2.0
-$firstP=TRUE;
-foreach($params as $x => $xVal) {
-    if($x=='val'){
-		if($xVal==1){
-			$xVal=0;
-			$cssST='btn btn-success btn-xs';
-			$txtST='<span class="glyphicon glyphicon-ok"></span>';
-		}else{
-			$xVal=1;
-			$cssST='btn btn-warning btn-xs';
-			$txtST='<span class="glyphicon glyphicon-remove"></span>';
+
+function genStatus($dest,$params,$css=NULL,$icons=NULL){//duotics_lib->v.4.1
+	$firstP=TRUE;
+	foreach($params as $x => $xVal) {
+		if($x=='val'){
+			if(!$icons){
+				if($xVal==1){
+					$xVal=0;
+					$cssST='btn btn-success btn-xs';
+					if($icons[1]) $txtST=$icons[1];
+					else $txtST='<i class="fas fa-check"></i>';
+				}else{
+					$xVal=1;
+					$cssST='btn btn-warning btn-xs';
+					if($icons[1]) $txtST=$icons[1];
+					else $txtST='<i class="fas fa-times"></i>';
+				}
+			}else{
+				foreach($icons as $y => $yVal){
+					if($xVal==$yVal[id]){
+						$cssST=$yVal[css];
+						$txtST=$yVal[icon];
+					}
+				}
+			}
 		}
+		if($firstP==TRUE){
+			$lP.='?'.$x.'='.$xVal;
+			$firstP=FALSE;
+		}else $lP.='&'.$x.'='.$xVal;
 	}
-	if($firstP==TRUE){
-		$lP.='?'.$x.'='.$xVal;
-		$firstP=FALSE;
-	}else $lP.='&'.$x.'='.$xVal;
-}
-$st='<a href="'.$dest.$lP.'" class="'.$cssST.' '.$css.'">'.$txtST.'</a>';
-return $st;
+	if($dest) $st='<a href="'.$dest.$lP.'" class="'.$cssST.' '.$css.'">'.$txtST.'</a>';
+	else $st='<span class="'.$cssST.' '.$css.'">'.$txtST.'</span>';
+	return $st;
 }
 /*HOW TO USE*/
-$btnStat=genStatus('_fncts.php',array('ids'=>$ids, 'val'=>$dRS['typEst'],'acc'=>md5('STc'),"url"=>$urlc));
+$btnStat=genStatus('_fncts.php',array('ids'=>$ids, 'val'=>$dRS['typEst'],'acc'=>md5('STc'),"url"=>$urlc),NULL,NULL);//Without custom icons
+$icons=array(
+	array("id"=>0,'css'=>'btn btn-success btn-sm',"icon"=>'<i class="fas fa-star"></i>'),
+	array("id"=>1,'css'=>'btn btn-warning btn-sm',"icon"=>'<i class="fas fa-star"></i>'),
+);
+$btnStat=genStatus('_fncts.php',array('ids'=>$ids, 'val'=>$dRS['typEst'],'acc'=>md5('STc'),"url"=>$urlc),NULL,$icons);//With custom icons
 ?>
