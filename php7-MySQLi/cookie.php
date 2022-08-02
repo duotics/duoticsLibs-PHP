20643e2a207ff4b0b10538d8bf9cd4c01bd7f5fc
Daniel Banegas <dbanegasl@gmail.com>
2022-08-02 16:39:54 -0500

    delete files php not used

--- php7-MySQLi/cookie.php
+++
@@ -1,5 +0,0 @@
-<?php include('../../init.php');
-$data=$_REQUEST;//RECEIVE DATA
-$LOG.=setCookieArray($data['cookieName'],$data['cookieID'],NULL,TRUE,$data['cookieNum']);
-echo json_encode(array("LOG"=>$LOG));
-?>
